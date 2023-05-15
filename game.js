bdf8a378c68d8da25972e69c99c2eee6516b2f47
LawrenciaApp <122866077+LawrenciaApp@users.noreply.github.com>
2023-05-15 14:09:05 +0000

    updated

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
 function getComputerChoice() {
-  
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomChoice = choices[Math.floor(Math.random() * 3)];
+  return randomChoice;
+}
+
+function gameRound(playerSelction, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if(playerSelection === computerSelection.toLowerCase()) {
+    return "It's a tie!";
+  }
+    else if(playerSelction === 'Rock' && computerSelection === 'Scissors') || (playerSelction === 'Paper' && computerSelection === 'Rock') ||  (playerSelction === 'Scissors' && computerSelection === 'Paper') {
+    return `You win ${playerSelection} beats ${computerSelction}.`;
+    }
 }
