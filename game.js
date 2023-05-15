b14ade1d7a7abb394e4723a5a8e64c6d10f6acdc
LawrenciaApp <122866077+LawrenciaApp@users.noreply.github.com>
2023-05-15 14:42:44 +0000

    updated

--- game.js
+++ game.js
@@ -13,3 +13,31 @@ function playRound(playerSelction, computerSelection) {
     return `You win ${playerSelection} beats ${computerSelction}.`;
     }
 }
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for(let i = 0;i < 5; i++) {
+    const playerSelection = prompt('Rock,paper or scissors?');
+    const computerSelection = computerPlay();
+    const roundResult= const playRound(playerSelection, computerSelection);
+    if(roundResult.startsWith(`You win`))
+    {
+      playerScore++;
+    } 
+    else if(roundResults.startsWith(`You lose!`)){
+      compurtScore++
+    }
+    console.log(`Round ${i + 1}: ${roundResult}`);
+  }
+  if(playerScore > computerScore) {
+    console.log(`You win the game! ${playerScore} to ${computerScore}.`);
+  }
+  else if(computerScore > playerScore) {
+    consloe.log(`You lose the game! ${computerScore} to ${playerScore}.`);
+  }
+  else {console.log(`It's a tie game! ${playerScore} to ${computerScore}`.);
+       }
+  }
+    
